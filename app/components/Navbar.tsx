19158a575c8e434134e7cccb72e707fcdab4908e
brendan-sadlier <brendan.sadlier@ucdconnect.ie>
2024-05-24 00:29:26 +0100

    Initial Commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ThemeToggle } from "./ThemeToggle";
+
+export function Navbar() {
+  return (
+
+    <nav className="border-b bg-background h-[10vh] flex items-center">
+
+      <div className="container flex items-center justify-between">
+        <Link href="/">
+          <h1 className="font-bold text-3xl">Reflect<span className="text-primary">ify</span></h1>
+        </Link>
+
+        <div className="flex items-center gap-x-5">
+          <ThemeToggle />
+
+          <div className="flex items-center gap-x-5">
+            <Button>Sign In</Button>
+            <Button variant="secondary">Sign Up</Button>
+          </div>      
+
+        </div>
+      </div>
+    </nav>
+
+  )
+}
