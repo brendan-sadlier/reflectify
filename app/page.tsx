52156a45f9ea26522cd3ad5841459ce72baa6fbf
brendan-sadlier <brendan.sadlier@ucdconnect.ie>
2024-05-24 01:06:42 +0100

    Homepage Created

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,45 @@
+import { Button } from "@/components/ui/button";
+
+import {RegisterLink} from "@kinde-oss/kinde-auth-nextjs/components";
+
+
 export default function Home() {
   return (
-    <>
-    </>
+    <section className="flex items-center justify-center bg-background h-[90vh]">
+
+      <div className="relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
+
+        <div className="max-w-3xl mx-auto text-center">
+
+          <div>
+            <span className="w-auto px-6 py-3 rounded-full bg-secondary">
+
+              <span className="text-sm font-medium text-primary">
+                Transform your thoughts into growth with our intuitive mood tracking and journaling tool.
+              </span>
+            </span>
+
+            <h1 className="mt-8 text-3xl font-extrabold tracking-tight lg:text-6xl text-secondary-foreground">Organise Your Thoughts with Ease</h1>
+            <p className="max-w-xl mx-auto mt-8 text-base">
+              Unlock self-awareness with our mood tracking and journaling tool. 
+              Easily record your emotions, reflect on your day, and gain insights into your mental well-being. 
+              Start your journey towards a healthier, happier you today.
+            </p>
+          </div>
+
+          <div className="flex justify-center max-w-sm mx-auto mt-10">
+
+            <RegisterLink>
+              <Button size="lg" className="w-full">Sign Up Today</Button>
+            </RegisterLink>
+
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
   );
 }
